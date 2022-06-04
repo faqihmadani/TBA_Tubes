7097e10733aa096b2901ab0015a1d1e010b00e5e
Faqih Madani <faqihfaqih22@gmail.com>
2022-06-04 07:34:36 +0700

    small fix

--- script.js
+++ script.js
@@ -143,6 +143,7 @@ const checkSentence = (sentence) => {
 
         // Parser
         parserTitle.className = 'block font-medium text-lg'
+        let realSentence = sentence
         sentence = sentence.replace(/\s+/g, ' ').trim()
         let tokens = sentence.toLowerCase().split(" ")
         tokens.push('EOS')
@@ -242,17 +243,16 @@ const checkSentence = (sentence) => {
 
         // Conclusion
         if (symbol == 'EOS' && stack.length == 0) {
-            resultParser.innerText = resultParser.innerText + 'Input string "' + sentence + '" diterima, sesuai Grammar \n'
+            resultParser.innerText = resultParser.innerText + 'Input string "' + realSentence + '" diterima, sesuai Grammar \n'
             resultParser.style.color = 'green'
         } else {
-            resultParser.innerText = resultParser.innerText + 'Error, input string "' + sentence + '" tidak diterima, tidak sesuai Grammar \n'
+            resultParser.innerText = resultParser.innerText + 'Error, input string "' + realSentence + '" tidak diterima, tidak sesuai Grammar \n'
             resultParser.style.color = 'red'
         }
     }
 }
 
 let form = document.getElementById('form')
-let button = document.getElementById('checkButton')
 
 const handleSubmit = (e) => {
     let sentence = document.getElementById('sentence').value
